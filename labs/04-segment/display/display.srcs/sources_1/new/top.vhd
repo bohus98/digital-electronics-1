0d96508eafe5ec3b5c85784dedc1d4c0d623ae00
Matúš Bohucký <58601109+bohus98@users.noreply.github.com>
2022-03-15 09:49:21 +0100

    Update top.vhd

--- labs/04-segment/display/display.srcs/sources_1/new/top.vhd
+++ labs/04-segment/display/display.srcs/sources_1/new/top.vhd
@@ -53,18 +53,18 @@ begin
     -- Experiments on your own: LED(7:4) indicators
 
     -- Turn LED(4) on if input value is equal to 0, ie "0000"
-    -- LED(4) <= `0` when WRITE YOUR CODE HERE
-    --LED(4)    <= '0' when (SW =0000 );
-              
-    -- Turn LED(5) on if input value is greater than "1001", ie 10, 11, 12, ...
-    -- LED(5) <= WRITE YOUR CODE HERE
-    --LED(5)    <= '0' when (SW > 1001);
+LED(4)  <= '1' when (SW = "0000") else '0';
 
-    -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
-    -- LED(6) <= WRITE YOUR CODE HERE
+-- Turn LED(5) on if input value is greater than "1001", ie 10, 11, 12, ...
+LED(5)  <= '1' when (SW > "1001") else '0';
 
-    -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
-    -- LED(7) <= WRITE YOUR CODE HERE
+-- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
+--                          1              3              5              7              9            b(11)         d(13)           F(15)
+LED(6) <= '1' when (SW = "0001" or SW = "0011" or SW = "0101" or SW = "0111" or SW = "1001" or SW = "1011" or SW = "1101" or SW = "1111") else '0';
+
+-- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
+--                          2               2              4              8   
+LED(7)  <= '1' when (SW = "0001" or SW = "0010" or SW = "0100" or SW = "1000") else '0';
 
 
 end Behavioral;
