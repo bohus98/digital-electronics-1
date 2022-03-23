33e69ed15e3820da6916e46b71c3817a079e8bd4
student <student@PC-626.urel.feec.vutbr.cz>
2022-03-23 10:14:01 +0100

    init

---
+++ labs/06-counters/counter/counter.srcs/sources_1/new/hex2seg.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/23/2022 09:45:42 AM
+-- Design Name: 
+-- Module Name: hex2seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hex2seg is
+--  Port ( );
+end hex2seg;
+
+architecture Behavioral of hex2seg is
+
+begin
+
+
+end Behavioral;
